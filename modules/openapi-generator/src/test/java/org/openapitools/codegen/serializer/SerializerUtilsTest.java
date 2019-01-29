fe3b71fb5b4b13c50c753c341b4c511988be48f6
Phil Adams <phil_adams@us.ibm.com>
2019-01-29 15:05:41 -0600

    Use internal copy of swagger-core modules

--- modules/openapi-generator/src/test/java/org/openapitools/codegen/serializer/SerializerUtilsTest.java
+++ modules/openapi-generator/src/test/java/org/openapitools/codegen/serializer/SerializerUtilsTest.java
@@ -48,47 +48,47 @@ public class SerializerUtilsTest {
         openAPI.addExtension("x-other", "value2");
 
         String content = SerializerUtils.toYamlString(openAPI);
-        String expected = "openapi: 3.0.1\n" + 
-               "info:\n" + 
-               "  description: Some description\n" + 
-               "  title: Some title\n" + 
-               "externalDocs:\n" + 
-               "  description: a-description\n" + 
-               "  url: http://abcdef.com\n" + 
-               "servers:\n" + 
-               "- description: first server\n" + 
-               "  url: http://www.server1.com\n" + 
-               "- description: second server\n" + 
-               "  url: http://www.server2.com\n" + 
-               "security:\n" + 
-               "- some_auth:\n" + 
-               "  - write\n" + 
-               "  - read\n" + 
-               "tags:\n" + 
-               "- description: some 1 description\n" + 
-               "  name: tag1\n" + 
-               "- description: some 2 description\n" + 
-               "  name: tag2\n" + 
-               "- description: some 3 description\n" + 
-               "  name: tag3\n" + 
-               "paths:\n" + 
-               "  /ping/pong:\n" + 
-               "    get:\n" + 
-               "      description: Some description\n" + 
-               "      operationId: pingOp\n" + 
-               "      responses:\n" + 
-               "        200:\n" + 
-               "          description: Ok\n" + 
-               "components:\n" + 
-               "  schemas:\n" + 
-               "    SomeObject:\n" + 
-               "      description: An Obj\n" + 
-               "      properties:\n" + 
-               "        id:\n" + 
-               "          type: string\n" + 
-               "      type: object\n" + 
-               "x-other: value2\n" + 
-               "x-custom: value1\n";
+        String expected = "openapi: 3.0.1\n" +
+               "info:\n" +
+               "  description: Some description\n" +
+               "  title: Some title\n" +
+               "externalDocs:\n" +
+               "  description: a-description\n" +
+               "  url: http://abcdef.com\n" +
+               "servers:\n" +
+               "- description: first server\n" +
+               "  url: http://www.server1.com\n" +
+               "- description: second server\n" +
+               "  url: http://www.server2.com\n" +
+               "security:\n" +
+               "- some_auth:\n" +
+               "  - write\n" +
+               "  - read\n" +
+               "tags:\n" +
+               "- description: some 1 description\n" +
+               "  name: tag1\n" +
+               "- description: some 2 description\n" +
+               "  name: tag2\n" +
+               "- description: some 3 description\n" +
+               "  name: tag3\n" +
+               "paths:\n" +
+               "  /ping/pong:\n" +
+               "    get:\n" +
+               "      description: Some description\n" +
+               "      operationId: pingOp\n" +
+               "      responses:\n" +
+               "        200:\n" +
+               "          description: Ok\n" +
+               "components:\n" +
+               "  schemas:\n" +
+               "    SomeObject:\n" +
+               "      description: An Obj\n" +
+               "      properties:\n" +
+               "        id:\n" +
+               "          type: string\n" +
+               "      type: object\n" +
+               "x-custom: value1\n" +
+               "x-other: value2\n";
         assertEquals(content, expected);
     }
 
@@ -105,19 +105,19 @@ public class SerializerUtilsTest {
                 .responses(new ApiResponses().addApiResponse("200", new ApiResponse().description("Ok")))));
 
         String content = SerializerUtils.toYamlString(openAPI);
-        String expected = "openapi: 3.0.1\n" + 
-                "info:\n" + 
-                "  title: Some title\n" + 
-                "servers:\n" + 
-                "- url: http://www.server1.com\n" + 
-                "paths:\n" + 
-                "  /ping/pong:\n" + 
-                "    get:\n" + 
-                "      description: Some description\n" + 
-                "      operationId: pingOp\n" + 
-                "      responses:\n" + 
-                "        200:\n" + 
-                "          description: Ok\n"; 
+        String expected = "openapi: 3.0.1\n" +
+                "info:\n" +
+                "  title: Some title\n" +
+                "servers:\n" +
+                "- url: http://www.server1.com\n" +
+                "paths:\n" +
+                "  /ping/pong:\n" +
+                "    get:\n" +
+                "      description: Some description\n" +
+                "      operationId: pingOp\n" +
+                "      responses:\n" +
+                "        200:\n" +
+                "          description: Ok\n";
         assertEquals(content, expected);
     }
 }
